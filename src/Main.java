affa8168feca7ecc9f09149ab4601cef5d3fe68a
koba98 <242072@astanait.edu.kz>
2025-04-27 22:40:57 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,43 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import HashTable.MyHashTable;
+import test.MyTestingClass;
+
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        MyHashTable<MyTestingClass, Student> table = new MyHashTable<>(100);
+        Random random = new Random();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        for (int i = 0; i < 10000; i++) {
+            int f1 = random.nextInt(1000);
+            int f2 = random.nextInt(1000);
+            String f3 = generateRandomString(random, 5);
+            MyTestingClass key = new MyTestingClass(f1, f2, f3);
+            Student value = new Student("Student" + i, i);
+
+            table.put(key, value);
         }
+
+        for (int i = 0; i < 100; i++) {
+            int count = 0;
+            MyHashTable.HashNode<MyTestingClass, Student> head = table.getBucketHead(i);
+            while (head != null) {
+                count++;
+                head = head.getNext();
+            }
+            System.out.println("Bucket " + i + ": " + count + " elements");
+        }
+
     }
+
+    private static String generateRandomString(Random random, int length) {
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            sb.append(alphabet.charAt(random.nextInt(alphabet.length())));
+        }
+        return sb.toString();
+    }
+
 }
